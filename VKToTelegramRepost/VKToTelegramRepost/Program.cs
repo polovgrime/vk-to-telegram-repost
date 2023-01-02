419c2c8b23e072b85843c91ef69322239b56c221
polovgrime <polovgrime@gmail.com>
2023-01-02 13:43:18 +0300

    Initial project

---
+++ VKToTelegramRepost/VKToTelegramRepost/Program.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Runtime.CompilerServices;
+using VkNet;
+using VkNet.Model;
+using VkNet.Model.RequestParams;
+
+namespace VKToTelegramRepost
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Test");
+            var settings = JsonConvert.DeserializeObject<SettingsDto>(File.ReadAllText("Settings.json"))!;
+            var api = new VkApi();
+
+            api.Authorize(new ApiAuthParams
+            {
+                AccessToken = settings.Token
+            });
+
+            var res = api.Groups.Get(new GroupsGetParams());
+
+            Console.WriteLine(res.TotalCount);
+
+            Console.ReadLine();
+        }
+    }
+}
